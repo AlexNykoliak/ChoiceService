2a6671aebbea94c6ff3fb8800cc040364bbcb2c5
OLeksandr Nykolyak <nykolyak.o@gmail.com>
2022-10-23 17:09:53 +0300

    settings&models

---
+++ backend/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Restaurant(models.Model):
+    name = models. CharField(max_length=25, verbose_name='restaurant_name', help_text='write the choice the name')
+    menu = models.ImageField(upload_to='static/menu')
+    publication_date = models.DateField(format('%Y-%m-%d'), auto_now_add=True)
+
+    def __str__(self):
+        return "{}, {}".format(self.name, self.publication_date)
+
