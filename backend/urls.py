8b9e8dfb2031a3a5916e5f7aa1de069251a5ef6a
OLeksandr Nykolyak <nykolyak.o@gmail.com>
2022-10-25 14:33:55 +0300

    add JWT

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('employee/', views.EmployeeList.as_view()),
@@ -10,6 +11,8 @@ urlpatterns = [
     path('restaurant/<int:restaurant_pk>/<int:user_pk>/vote/', views.RestaurantVoteView.as_view()),
     path('menu/', views.MenuView.as_view()),
     path('results/', views.ResultsView.as_view()),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
